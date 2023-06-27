c470e869a2a2a99571fc5fce5acd2518d2c84e01
Jason <jason@Jasons-MacBook-Air.local>
2023-06-27 08:15:37 -0700

    Adding bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -9,6 +9,12 @@ function error404(){
                 <p>
                     Oops, sorry, we can't find this page!
                 </p>
+                <div>
+                    <img src="/images/birdie.jpg" alt ="birdie"/>
+                    <div>
+                    Photo by <a href="https://unsplash.com/@miklevasilyev?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mikhail Vasilyev</a> on <a href="https://unsplash.com/photos/gGC63oug3iY?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
             </main>
         </Def>
     )
