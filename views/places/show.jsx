a306a858dd7c08cedd734ca21fde43470417acea
Jason <jason@Jasons-MacBook-Air.local>
2023-07-11 22:43:06 -0700

    helper methods on show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,15 +5,19 @@ function show(data){
     return(
         <Def>
             <main> 
+                <div className ="row">
+                <div className ="col-sm-12">
                 <h1>{data.place.name}</h1>
                 <img src ={data.place.pic} alt={data.place.name}/>
+                <h3>Located in {data.place.city}, {data.place.state}</h3>
+                </div>
+                </div>
+                <div className='col-sm-12'>
                 <h2>Rating</h2>
-                <p>not rated</p>
+                <h4>Not rated</h4>
                 <h2>Description</h2>
-                <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
-                <hr/>
-                <h2>Comments?</h2>
-                <p>no comments yet.</p>
+                <h4>{data.place.showEstablished()}</h4>
+                <h4>Serving {data.place.cuisines}</h4>
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                     Edit
                 </a>  
@@ -21,8 +25,13 @@ function show(data){
                     <button type="submit" className="btn btn-danger">
                         Delete
                     </button>
-                </form>     
-
+                    </form>
+                </div>
+                <hr/>
+                <div className="col-sm-12">
+                <h2>Comments?</h2>
+                <h4>No comments yet.</h4>
+                </div>
             </main>
         </Def>
     )
