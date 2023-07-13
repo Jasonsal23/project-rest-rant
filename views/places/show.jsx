872ce42eca26eb6fb8876b61e343d14e55c52d96
Jason <jason@Jasons-MacBook-Air.local>
2023-07-13 14:07:33 -0700

    comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React=require('react')
 const Def=require('../default')
 
 function show(data){
+    let comments =(
+        <h3 className='inactive'>
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+            <div className="border">
+                <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+                <h4>{c.content}</h4>
+                <h3>
+                <stong>- {c.author}</stong>
+                </h3>
+                <h4>Rating: {c.stars}</h4>
+            </div>
+            )
+        })
+    }
     return(
         <Def>
             <main> 
@@ -30,7 +49,7 @@ function show(data){
                 <hr/>
                 <div className="col-sm-12">
                 <h2>Comments?</h2>
-                <h4>No comments yet.</h4>
+                {comments}
                 </div>
             </main>
         </Def>
